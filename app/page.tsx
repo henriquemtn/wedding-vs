11bde717d7a9c23d2215b6f59a4453bc1b222f22
henriquemtn <henriquemartinsilveira2@gmail.com>
2025-02-22 13:08:42 -0300

    update: removing countdown

--- app/page.tsx
+++ app/page.tsx
@@ -51,8 +51,7 @@ export default function Home() {
           <div
             className="flex flex-col justify-start w-full md:w-1/2">
             <h3 className="text-[#755955] mb-4 text-sm font-semibold tracking-wider">SOBRE O EVENTO</h3>
-            <h2 className="text-black text-3xl mb-6 font-light">O Casamento será no Sitio Vitória</h2>
-            <Countdown />
+            <h2 className="text-black text-3xl font-light">O Casamento será no Sitio Vitória</h2>
             <ul className="flex flex-col gap-4 mt-8">
               <li className="flex items-center">
                 <div className="flex items-center size-10">
